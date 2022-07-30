cfc5ab0cd9e8be95e241fa2313e5daede5198b5a
Aliaksandr Zuyeuski <negrustin87@gmail.com>
2022-07-30 17:39:51 +0200

    add TicketsTravelTimeComparator

---
+++ src/main/java/ru/netology/compararor/TicketsTravelTimeComparator.java
@@ -0,0 +1,17 @@
+package ru.netology.compararor;
+import ru.netology.domain.Ticket;
+
+import java.util.Comparator;
+
+public class TicketsTravelTimeComparator implements Comparator<Ticket> {
+    @Override
+    public int compare(Ticket o1, Ticket o2) {
+        if(o1.getTravelTime() > o2.getTravelTime()){
+            return 1;
+        } else if (o1.getTravelTime() < o2.getTravelTime()) {
+            return -1;
+        }else {
+            return 0;
+        }
+    }
+}
